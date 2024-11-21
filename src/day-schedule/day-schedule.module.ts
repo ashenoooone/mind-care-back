2afee8b1cf9c03b272e6b9b27b7a5465913fb10d
Roman Gontar <ashenoooone@gmail.com>
2024-11-21 22:02:36 +0400

    fix

--- src/day-schedule/day-schedule.module.ts
+++ src/day-schedule/day-schedule.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DayScheduleService } from './day-schedule.service';
 import { DayScheduleController } from './day-schedule.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   controllers: [DayScheduleController],
   providers: [DayScheduleService],
+  imports: [DatabaseModule],
 })
 export class DayScheduleModule {}
--- src/working-shedule/working-shedule.module.ts
+++ src/working-shedule/working-shedule.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WorkingSheduleService } from './working-shedule.service';
 import { WorkingSheduleController } from './working-shedule.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   controllers: [WorkingSheduleController],
   providers: [WorkingSheduleService],
+  imports: [DatabaseModule],
 })
 export class WorkingSheduleModule {}
