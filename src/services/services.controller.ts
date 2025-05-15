7751e1f3acd634d2b12516ec619e5f48f4870074
Roman Gontar <ashenoooone@gmail.com>
2025-05-15 23:36:44 +0400

    Удаление использования JwtAuthGuard в контроллере ServicesController для упрощения логики аутентификации.

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -7,16 +7,13 @@ import {
   Param,
   Delete,
   Query,
-  UseGuards,
 } from '@nestjs/common';
 import { ServicesService } from './services.service';
 import { GetServicesDto } from './dto/get-services.dto';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('services')
-@UseGuards(JwtAuthGuard)
 export class ServicesController {
   constructor(private readonly servicesService: ServicesService) {}
 
